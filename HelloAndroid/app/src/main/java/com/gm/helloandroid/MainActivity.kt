a58236f52c1400af1489684020ef2a65ba7d701e
Golam Mostofa <mostofart@gmail.com>
2023-10-20 09:33:10 +0600

    Starting Android with Hello Android.
    Initial commit. Hello android!

---
+++ HelloAndroid/app/src/main/java/com/gm/helloandroid/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.gm.helloandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.gm.helloandroid.ui.theme.HelloAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloAndroidTheme {
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "Hello Android!")
+                }
+            }
+        }
+    }
+}
