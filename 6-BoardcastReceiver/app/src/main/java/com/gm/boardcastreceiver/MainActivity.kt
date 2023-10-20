d990f01131697b709f8266f5f6ea65202cb4047c
Golam Mostofa <mostofart@gmail.com>
2023-10-20 12:31:16 +0600

    Project Structure Changed!
    Project Structure Changed
    Boardcast Receiver Added.

---
+++ 6-BoardcastReceiver/app/src/main/java/com/gm/boardcastreceiver/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.gm.boardcastreceiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gm.boardcastreceiver.ui.theme.BoardcastReceiverTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val airplaneModeReceiver = AirplaneModeReceiver()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        registerReceiver(airplaneModeReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+
+        setContent {
+            BoardcastReceiverTheme {
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(airplaneModeReceiver)
+    }
+}
