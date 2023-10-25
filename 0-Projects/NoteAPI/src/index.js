2e615d910668ae8e94007d46a77386e6c2e8f73e
Golam Mostofa <mostofart@gmail.com>
2023-10-25 15:57:21 +0600

    Working on NoteAPI

--- 0-Projects/NoteAPI/src/index.js
+++ 0-Projects/NoteAPI/src/index.js
@@ -1,1 +1,15 @@
-console.log("hello js");
+const express = require("express");
+const app = express();
+
+const userRouter = require("./routes/userRoutes");
+const noteRouter = require("./routes/noteRoutes");
+
+
+
+app.get("/", (req,res) =>{
+    res.send("fuck you");
+});
+
+app.listen(5000, ()=>{
+    console.log("Server started on port 5000");
+});
